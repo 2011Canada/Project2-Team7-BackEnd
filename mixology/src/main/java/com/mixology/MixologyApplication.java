d5021512d95216294b4100f61a47c2681b2c34d8
Rous Epistola <rousbepistola@yahoo.com>
2021-01-08 18:52:14 -0500

    Initial setup for spring boot

---
+++ mixology/src/main/java/com/mixology/MixologyApplication.java
@@ -0,0 +1,13 @@
+package com.mixology;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MixologyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MixologyApplication.class, args);
+	}
+
+}
---
+++ mixology/src/test/java/com/mixology/MixologyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mixology;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MixologyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
